e2cd37fc3b3082950ab0cd57b486c4923bca5f5a
Yuji Nagafuchi <yuji@YujinoMacBook-Air.local>
2023-04-18 22:35:32 +0900

    完了未完了の実装が完了しました。

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ $("#addToDo").click(function () {
 
     $("input").val("");
 })
+
+$(document).on("change", "Input[type=checkbox]", function() {
+    if($(this).is(":checked")){
+        $(this).parent().css("text-decoration", "line-through");
+    }else{
+        $(this).parent().css("text-decoration", "none");
+    }
+})

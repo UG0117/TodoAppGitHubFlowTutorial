2912725c22f399ad406ea93461430da170034471
Yuji Nagafuchi <yuji@YujinoMacBook-Air.local>
2023-04-18 22:40:18 +0900

    色の修正を実施しました。

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ $("#addToDo").click(function () {
 $(document).on("change", "Input[type=checkbox]", function() {
     if($(this).is(":checked")){
         $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "#ccc");
     }else{
         $(this).parent().css("text-decoration", "none");
+        $(this).parent().css("color", "#000");
     }
 })
